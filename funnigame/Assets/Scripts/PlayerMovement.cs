c5bded941b3c134011cb9703ded2ae949f1e30ea
Nick Bohnsack <nick.bohnsack@gmail.com>
2022-04-25 23:29:42 +0200

    Implemented Player Movement
    Quite Extensively Tweakable through values in inspector

--- funnigame/Assets/Scripts/PlayerMovement.cs
+++ funnigame/Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,42 @@ using UnityEditor;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
-    CharacterController2D controller;
 
-    [SerializeField] string groundTag; 
+    [Header("Movement")]
     [SerializeField] float movementSpeed; 
+    [SerializeField] float acceleration;
+    [SerializeField] float decceleration;
+    [SerializeField] float velPower;
+    [SerializeField] float frictionAmount;
+    [Space]
+
+
+    [Header("Jumping")]
     [SerializeField] float jumpForce;
+    [SerializeField] float jumpCoyoteTime;
+    [SerializeField] float jumpBufferTime;
+    [SerializeField] float jumpCutMultiplier;
+    [SerializeField] float fallGravityMultiplier;
+    [SerializeField] Transform groundCheckPoint;
+    [SerializeField] Vector2 groundCheckSize;
+    [SerializeField] private int groundLayerID;
+
+
+    private float gravityScale;
+
+
+    //state variables
+    bool isJumping;
+    bool jumpInputReleased;
+
+
+    //timers
+    private float lastGroundTime;
+    private float lastJumpTime;
+    private float lastPressedJumpTime;
+
+
+
 
     //get horizontal input value (a/d or left/right)
     private float horizontalInput;
@@ -19,12 +50,18 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        controller = GetComponent<CharacterController2D>();
+        gravityScale = rb.gravityScale;
     }
 
     void Update()
     {
+        //update timers
+        UpdateTimers();
+        //set horizontal input to current input
         horizontalInput = Input.GetAxis("Horizontal");
+        //check for/handle inputs
+        InputHandler();
+        GroundCheck();
     }
 
 
@@ -34,26 +71,93 @@ public class PlayerMovement : MonoBehaviour
         MovePlayer();
     }
 
-    void MovePlayer()
+    private void UpdateTimers()
     {
-        jump = Input.GetButtonDown("Jump");
-        //move the player
-        controller.Move(horizontalInput * movementSpeed * Time.fixedDeltaTime, false, Jump());
-        //jump = false;
+        lastGroundTime -= Time.deltaTime;
+        lastJumpTime -= Time.deltaTime;
     }
 
 
-    private bool Jump()
+    private void MovePlayer()
     {
-        //check if jump key is pressed
+        //calculate direction and speed of movement
+        float targetSpeed = horizontalInput * movementSpeed;
+        //calculate difference between current velocity and desired velocity
+        float speedDif = targetSpeed - rb.velocity.x;
+        //change acceleration/decceleration
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
+
+        float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
+        //apply movement
+        rb.AddForce(movement * Vector2.right);
+
+        if (lastGroundTime > 0 && Mathf.Abs(horizontalInput) < 0.01f)
+        {
+            float amount = Mathf.Min(Mathf.Abs(rb.velocity.x), Mathf.Abs(frictionAmount));
+            amount *= Mathf.Sign(rb.velocity.x);
+            rb.AddForce(Vector2.right * -amount, ForceMode2D.Impulse);
+        }
+
+        if (lastGroundTime > 0 && lastJumpTime > 0 && !isJumping)
+        {
+            Jump();
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = gravityScale * fallGravityMultiplier;
+        }
+        else
+        {
+            rb.gravityScale = gravityScale;
+        }
+    }
+
+    
+    private void GroundCheck()
+    {
+        //return true if grounded
+        if (Physics2D.OverlapBox(groundCheckPoint.position, groundCheckSize, 0, groundLayerID))
+        {
+            lastGroundTime = jumpCoyoteTime;
+            isJumping = false;
+        }
+    }
+
+
+    private void Jump()
+    {
+        //apply force in impulse
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        //reset timers/set state variables
+        lastGroundTime = 0;
+        lastJumpTime = 0;
+        isJumping = true;
+        jumpInputReleased = false;
+    }
+
+    private bool CanJump()
+    {
+        return lastGroundTime > 0 && !isJumping;
+    }
+
+
+    private void InputHandler()
+    {
         if (Input.GetButtonDown("Jump"))
         {
-            //if yes, return true
-            return true;
+            lastJumpTime = jumpBufferTime;
         }
-        //if not, return false
-        return false;
+        else if (Input.GetButtonUp("Jump"))
+        {
+            //implement jump cut
+            if (rb.velocity.y > 0 && isJumping)
+            {
+                rb.AddForce(Vector2.down * rb.velocity.y * (1 - jumpCutMultiplier), ForceMode2D.Impulse);
+            }
+            jumpInputReleased = true;
+            lastJumpTime = 0;
+        }
     }
-    
 
 }
